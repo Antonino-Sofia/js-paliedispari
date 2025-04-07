f106ba043205b8fc0d3d7e62f1fa1be13255db4b
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-07 16:52:54 +0200

    js-paliedispari/ pari-e-dispari/ variabili

---
+++ pari dispari/main.js
@@ -0,0 +1,18 @@
+//Pari e Dispari
+//1-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//2-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//3-Sommiamo i due numeri
+//4-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//5-Dichiariamo chi ha vinto.
+
+//chiedo all'utente pari o dispari e faccio una variabile
+const userChoice = prompt("Scegli tra pari e dispari");
+console.log({ userChoice });
+
+if (userChoice === "pari" || userChoice === "dispari");
+
+//chiedo il numero da scegliere e faccio una variabile
+const userNumber = parseInt(prompt("Inserisci un numero tra 1 e 5"));
+
+if (userNumber >= 1 && userNumber <= 5);
+console.log({ userNumber });
